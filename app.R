9c6e3c903f7779da8c500e0d931994b201500390
cedarwarman <cedardalewarman@gmail.com>
2022-08-16 10:44:30 -0400

    Make some progress towards correct table x and y.

--- app.R
+++ app.R
@@ -1,26 +1,33 @@
 library(shiny)
 library(readxl)
 library(bslib)
+library(DT)
 
 # Loading the accession metadata
 accessions <- read_excel(file.path(getwd(), "data", "accessions.xlsx"))
 
-# ui <- bootstrapPage(
-#   div(class = "container-fluid",
-#     div(class = "row",
-#       align = "center",
-#       dataTableOutput("accessions_table")
-#     )
-#   )  
-# )
-
-ui <- fluidPage(
-  dataTableOutput("accessions_table")
+ui <- bootstrapPage(
+  div(class = "container-fluid",
+    div(class = "row justify-content-center",
+      align = "center",
+      div(class = "col-xl-4",
+          DTOutput("accessions_table", width = "100%")
+      )
+    )
+  )  
 )
+
+# ui <- fluidPage(
+#   dataTableOutput("accessions_table")
+# )
 
 server <- function(input, output, session) {
   theme  = bs_theme(version = 5)
-  output$accessions_table <- renderDataTable(accessions) 
+  output$accessions_table <- renderDT(accessions, options = list(scrollX = TRUE,
+                                                                 scrollY = "500px",
+                                                                 pageLength = 500,
+                                                                 # autoWidth = TRUE,
+                                                                 fixedHeader = TRUE)) 
 }
 
 shinyApp(ui, server)
