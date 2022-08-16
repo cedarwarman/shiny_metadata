4f874bcf06e22e8cdc53a464bc66fc78e92fb8c2
cedarwarman <cedardalewarman@gmail.com>
2022-08-16 11:02:55 -0400

    Minimal table with theme, search, correct scrolling.

--- app.R
+++ app.R
@@ -2,11 +2,14 @@ library(shiny)
 library(readxl)
 library(bslib)
 library(DT)
+library(shinythemes)
 
 # Loading the accession metadata
 accessions <- read_excel(file.path(getwd(), "data", "accessions.xlsx"))
 
 ui <- bootstrapPage(
+  theme = shinytheme("slate"),
+  # themeSelector(),
   div(class = "container-fluid",
     div(class = "row justify-content-center",
       align = "center",
@@ -23,11 +26,15 @@ ui <- bootstrapPage(
 
 server <- function(input, output, session) {
   theme  = bs_theme(version = 5)
-  output$accessions_table <- renderDT(accessions, options = list(scrollX = TRUE,
-                                                                 scrollY = "500px",
-                                                                 pageLength = 500,
-                                                                 # autoWidth = TRUE,
-                                                                 fixedHeader = TRUE)) 
+  output$accessions_table <- renderDT(accessions, 
+                                      style = "bootstrap",
+                                      options = list(scrollX = TRUE,
+                                                     scrollY = "500px",
+                                                     pageLength = 500,
+                                                     # autoWidth = TRUE,
+                                                     fixedHeader = TRUE, 
+                                                     dom = "ft"))
+                                                     # info = FALSE))
 }
 
 shinyApp(ui, server)
