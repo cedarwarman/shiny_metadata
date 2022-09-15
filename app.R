44ec0b5bac239ed052032835cf6af48b2d3df825
cedarwarman <cedardalewarman@gmail.com>
2022-09-14 21:25:30 -0700

    Add all plotly plots.

--- app.R
+++ app.R
@@ -15,7 +15,7 @@ flower_measurements <- read.table(file.path(getwd(), "data", "flower_measurement
 
 
 # Functions ---------------------------------------------------------------
-make_flower_plot <- function(plot_type, flower_df, row_selection) {
+make_flower_plot_old <- function(plot_type, flower_df, row_selection) {
   # Putting the theme here to make it easier to edit
   plot_theme <- theme(
     axis.title = element_text(size = 12, face = 'bold', color = 'white'),
@@ -192,32 +192,276 @@ make_plotly_flower_plot <- function(flower_df) {
                          x = reorder(flower_df$accession_id, flower_df$anther_over_pistil, median),
                          y = ~anther_over_pistil,
                          type = "box",
-                         fillcolor = "gray",
-                         line = list(width= 0.5,
+                         fillcolor = "#cccccc", # gray
+                         line = list(width = 1,
                                      color = "white"),
                          marker = list(size = 3,
                                        color = "white"))
   
   # Adding aesthetics
   output_plot <- output_plot %>% layout(title = list(text = "Anther and pistil lengths by accession",
-                                                     font = list(size = 18)),
+                                                     font = list(size = 22)),
                                         xaxis = list(title = F,
                                                      showline = T,
                                                      showticklabels = F,
-                                                     linewidth = 2,
+                                                     linewidth = 1,
                                                      linecolor = "white"),
                                         yaxis = list(title = list(text = "Ratio of AL / PL",
-                                                                  font = list(size = 12)),
+                                                                  font = list(size = 16),
+                                                                  standoff = 0),
                                                      range = list(0.6, 1.4),
                                                      showline = T,
                                                      showgrid = F,
-                                                     linewidth = 2,
+                                                     linewidth = 1,
                                                      linecolor = "white",
                                                      tickfont = list(size = 10)),
                                         font = list(family = "Arial Black",
                                                     color = "white"),
                                         paper_bgcolor = "#060606",
-                                        plot_bgcolor = "#060606")
+                                        plot_bgcolor = "#060606",
+                                        margin = list(t = 50, r = 30))
+  
+  # Removing toolbar
+  output_plot <- output_plot %>% config(displayModeBar = F)
+  
+  return(output_plot)
+}
+
+make_flower_plot <- function(plot_type, flower_df, row_selection) {
+  if (plot_type == "ratio") {   
+    if (length(row_selection)) {
+      # Making new column for if it's selected or not
+      selected_accessions <- accessions$name_CW[row_selection]
+      flower_df$row_selected <- NA
+      flower_df$row_selected[flower_df$accession_id %in% selected_accessions] <- "selected"
+      flower_df$row_selected[is.na(flower_df$row_selected)] <- "not_selected"
+      
+      # Making the plot 
+      output_plot <- plot_ly(data = flower_df,
+                             x = reorder(flower_df$accession_id, flower_df$anther_over_pistil, median),
+                             y = ~anther_over_pistil,
+                             color = ~row_selected,
+                             colors = c("white", "magenta"),
+                             type = "box",
+                             line = list(width = 1),
+                             marker = list(size = 3))
+      
+      # Adding aesthetics
+      output_plot <- output_plot %>% layout(title = list(text = "Anther and pistil lengths by accession",
+                                                         font = list(size = 22)),
+                                            xaxis = list(title = F,
+                                                         showline = T,
+                                                         showticklabels = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white"),
+                                            yaxis = list(title = list(text = "Ratio of AL / PL",
+                                                                      font = list(size = 16),
+                                                                      standoff = 0),
+                                                         range = list(0.6, 1.4),
+                                                         showline = T,
+                                                         showgrid = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white",
+                                                         tickfont = list(size = 10)),
+                                            font = list(family = "Arial Black",
+                                                        color = "white"),
+                                            paper_bgcolor = "#060606",
+                                            plot_bgcolor = "#060606",
+                                            margin = list(t = 50, r = 30),
+                                            showlegend = F)
+      
+    } else {
+      # Making the plot
+      output_plot <- plot_ly(data = flower_df,
+                             x = reorder(flower_df$accession_id, flower_df$anther_over_pistil, median),
+                             y = ~anther_over_pistil,
+                             type = "box",
+                             fillcolor = "#838383",
+                             line = list(width = 1,
+                                         color = "#ffffff"),
+                             marker = list(size = 3,
+                                           color = "#ffffff"))
+      
+      # Adding aesthetics
+      output_plot <- output_plot %>% layout(title = list(text = "Anther and pistil lengths by accession",
+                                                         font = list(size = 22)),
+                                            xaxis = list(title = F,
+                                                         showline = T,
+                                                         showticklabels = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white"),
+                                            yaxis = list(title = list(text = "Ratio of AL / PL",
+                                                                      font = list(size = 16),
+                                                                      standoff = 0),
+                                                         range = list(0.6, 1.4),
+                                                         showline = T,
+                                                         showgrid = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white",
+                                                         tickfont = list(size = 10)),
+                                            font = list(family = "Arial Black",
+                                                        color = "white"),
+                                            paper_bgcolor = "#060606",
+                                            plot_bgcolor = "#060606",
+                                            margin = list(t = 50, r = 30))
+    }
+  } else if (plot_type == "anther") {
+    if (length(row_selection)) {
+      # Making new column for if it's selected or not
+      selected_accessions <- accessions$name_CW[row_selection]
+      flower_df$row_selected <- NA
+      flower_df$row_selected[flower_df$accession_id %in% selected_accessions] <- "selected"
+      flower_df$row_selected[is.na(flower_df$row_selected)] <- "not_selected"
+      
+      # Making the plot 
+      output_plot <- plot_ly(data = flower_df,
+                             x = reorder(flower_df$accession_id, flower_df$anther_length, median),
+                             y = ~anther_length,
+                             color = ~row_selected,
+                             colors = c("white", "magenta"),
+                             type = "box",
+                             line = list(width = 1),
+                             marker = list(size = 3))
+      
+      # Adding aesthetics
+      output_plot <- output_plot %>% layout(title = list(text = "Anther lengths by accession",
+                                                         font = list(size = 22)),
+                                            xaxis = list(title = F,
+                                                         showline = T,
+                                                         showticklabels = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white"),
+                                            yaxis = list(title = list(text = "Anther length (mm)",
+                                                                      font = list(size = 16),
+                                                                      standoff = 0),
+                                                         range = list(5, 13),
+                                                         showline = T,
+                                                         showgrid = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white",
+                                                         tickfont = list(size = 10)),
+                                            font = list(family = "Arial Black",
+                                                        color = "white"),
+                                            paper_bgcolor = "#060606",
+                                            plot_bgcolor = "#060606",
+                                            margin = list(t = 50, r = 30),
+                                            showlegend = F)
+    } else {
+      # Making the plot
+      output_plot <- plot_ly(data = flower_df,
+                             x = reorder(flower_df$accession_id, flower_df$anther_length, median),
+                             y = ~anther_length,
+                             type = "box",
+                             fillcolor = "#838383",
+                             line = list(width = 1,
+                                         color = "#ffffff"),
+                             marker = list(size = 3,
+                                           color = "#ffffff"))
+      
+      # Adding aesthetics
+      output_plot <- output_plot %>% layout(title = list(text = "Anther lengths by accession",
+                                                         font = list(size = 22)),
+                                            xaxis = list(title = F,
+                                                         showline = T,
+                                                         showticklabels = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white"),
+                                            yaxis = list(title = list(text = "Anther length (mm)",
+                                                                      font = list(size = 16),
+                                                                      standoff = 0),
+                                                         range = list(5, 13),
+                                                         showline = T,
+                                                         showgrid = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white",
+                                                         tickfont = list(size = 10)),
+                                            font = list(family = "Arial Black",
+                                                        color = "white"),
+                                            paper_bgcolor = "#060606",
+                                            plot_bgcolor = "#060606",
+                                            margin = list(t = 50, r = 30))
+    }
+  } else {
+    if (length(row_selection)) {
+      # Making new column for if it's selected or not
+      selected_accessions <- accessions$name_CW[row_selection]
+      flower_df$row_selected <- NA
+      flower_df$row_selected[flower_df$accession_id %in% selected_accessions] <- "selected"
+      flower_df$row_selected[is.na(flower_df$row_selected)] <- "not_selected"
+      
+      # Making the plot 
+      output_plot <- plot_ly(data = flower_df,
+                             x = reorder(flower_df$accession_id, flower_df$pistil_length, median),
+                             y = ~pistil_length,
+                             color = ~row_selected,
+                             colors = c("white", "magenta"),
+                             type = "box",
+                             line = list(width = 1),
+                             marker = list(size = 3))
+      
+      # Adding aesthetics
+      output_plot <- output_plot %>% layout(title = list(text = "Pistil lengths by accession",
+                                                         font = list(size = 22)),
+                                            xaxis = list(title = F,
+                                                         showline = T,
+                                                         showticklabels = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white"),
+                                            yaxis = list(title = list(text = "Pistil length (mm)",
+                                                                      font = list(size = 16),
+                                                                      standoff = 0),
+                                                         range = list(5, 13),
+                                                         showline = T,
+                                                         showgrid = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white",
+                                                         tickfont = list(size = 10)),
+                                            font = list(family = "Arial Black",
+                                                        color = "white"),
+                                            paper_bgcolor = "#060606",
+                                            plot_bgcolor = "#060606",
+                                            margin = list(t = 50, r = 30),
+                                            showlegend = F)
+      
+    } else {
+      # Making the plot
+      output_plot <- plot_ly(data = flower_df,
+                             x = reorder(flower_df$accession_id, flower_df$pistil_length, median),
+                             y = ~pistil_length,
+                             type = "box",
+                             fillcolor = "#838383",
+                             line = list(width = 1,
+                                         color = "#ffffff"),
+                             marker = list(size = 3,
+                                           color = "#ffffff"))
+      
+      # Adding aesthetics
+      output_plot <- output_plot %>% layout(title = list(text = "Pistil lengths by accession",
+                                                         font = list(size = 22)),
+                                            xaxis = list(title = F,
+                                                         showline = T,
+                                                         showticklabels = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white"),
+                                            yaxis = list(title = list(text = "Pistil length (mm)",
+                                                                      font = list(size = 16),
+                                                                      standoff = 0),
+                                                         range = list(5, 13),
+                                                         showline = T,
+                                                         showgrid = F,
+                                                         linewidth = 1,
+                                                         linecolor = "white",
+                                                         tickfont = list(size = 10)),
+                                            font = list(family = "Arial Black",
+                                                        color = "white"),
+                                            paper_bgcolor = "#060606",
+                                            plot_bgcolor = "#060606",
+                                            margin = list(t = 50, r = 30)) 
+    }
+  }
+  
+  # Removing toolbar
+  output_plot <- output_plot %>% config(displayModeBar = F)
   
   return(output_plot)
 }
@@ -284,13 +528,15 @@ server <- function(input, output, session) {
    ))
    
   output$ratio_plot <- renderPlotly({
+    # make_flower_plot("ratio", flower_measurements, input$accessions_table_rows_selected)
+    # make_plotly_flower_plot(flower_measurements)
     make_flower_plot("ratio", flower_measurements, input$accessions_table_rows_selected)
   })
  
   output$anther_plot <- renderPlotly({
     make_flower_plot("anther", flower_measurements, input$accessions_table_rows_selected)
   })
-  
+
   output$pistil_plot <- renderPlotly({
     make_flower_plot("pistil", flower_measurements, input$accessions_table_rows_selected)
   })
